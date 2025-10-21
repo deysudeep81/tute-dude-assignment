4488417b7fe156497d348758c4e0381c82f5337f
deysudeep81 <deysudeep81@gmail.com>
2025-10-21 14:23:49 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,18 @@
+print("Welcome to Grading System")
+marks=int (input("Enter Your Marks \n"))
+if marks == 90:
+    Grade ="A"
+elif marks == 80:
+    Grade ="B"
+elif marks == 70:
+    Grade ="C"
+else:
+    print("Try Hard")
+        
+    
+
+
+
+    
+    
+        
